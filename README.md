fc5565831229dc9f632e9ffd9571d917392a8abe
luishmd <luis.hmd@gmail.com>
2019-06-08 20:00:51 +0100

    Improved readme and added print function.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Pi-calculator
+# Description
+Small script illustrating the application of a Monte Carlo method to determine the value of pi
+	
+	
+# Technologies used
+Python3
+
+# Status
+This work is completed.
+
+
+# How it works
+The idea is to generate randomly *N* points in a square. Then count the *C* number of points that fall inside the circle contained in the square (circle with the same diameter as the square).
+
+An approximation of Pi is then given by 4*C/N.
+
+
+# How to use it
+1. Open *pi_calulator* and edit the parameter *N*
--- pi_calculator.py
+++ pi_calculator.py
@@ -1,24 +1,47 @@
-__author__ = 'Luis'
+__author__ = 'Luis Domingues'
 
 import random
 import math
 
+#----------------------------------------------
+# INPUTS
+#----------------------------------------------
+N = 100
 
+
+
+#----------------------------------------------
+# FUNCTIONS
+#----------------------------------------------
 def pi(n):
+    """
+    Function that determines an approximation of pi using a Monte Carlo simulation
+    :param n: Number of random points
+    :return: approx. of pi or None
+    """
     if n > 1:
         c = 0
         for i in range(n):
             a = random.uniform(0,1)
             b = random.uniform(0,1)
             if math.sqrt((a-0.5)**2+(b-0.5)**2) < 0.5:
-                c +=1
-        return 4 * float(c) / float(n)
+                c += 1
+        return 4.0 * float(c) / float(n)
     else:
-        return 0
+        return None
+
+def print_results(pi, decimal_places=4):
+    """
+    Function that prints the reults
+    :param pi: approximation of pi
+    :return: 
+    """
+    if pi != None:
+        s = "Approximate value for Pi: {:." + str(decimal_places) + "f}"
+        print(s.format(pi))
+    else:
+        print("Could not determine pi")
 
 
 if __name__ == "__main__":
-    for i in range(20):
-        print(pi(i))
-
-    print(pi(1000))
+    print_results(pi(N))
